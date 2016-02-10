66b91f72f40e7c06fcdb9d5f3ed40a2c63abd9a7
Giles Colclough <giles.colclough@gmail.com>
2016-02-10 18:06:46 +0000

    Remove docs
    User can build themselves.
    
    Signed-off-by: Giles Colclough <giles.colclough@gmail.com>

--- hidden_state_inference/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['main',['main',['../hidden__state__inference__test_8cpp.html#af3ed9c200de85b53c94cd18764b246a2',1,'hidden_state_inference_test.cpp']]],
-  ['mexfunction',['mexFunction',['../hidden__state__inference__mx_8cpp.html#a6a215cbfde54f82a3ce599228fc3fce5',1,'hidden_state_inference_mx.cpp']]]
-];
--- hidden_state_inference/html/search/files_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['hidden_5fstate_5finference_2ecpp',['hidden_state_inference.cpp',['../hidden__state__inference_8cpp.html',1,'']]],
-  ['hidden_5fstate_5finference_5fmx_2ecpp',['hidden_state_inference_mx.cpp',['../hidden__state__inference__mx_8cpp.html',1,'']]],
-  ['hidden_5fstate_5finference_5ftest_2ecpp',['hidden_state_inference_test.cpp',['../hidden__state__inference__test_8cpp.html',1,'']]]
-];
