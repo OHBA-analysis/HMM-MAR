2cd87ef5bf2503101f2eaaecbf9bc7dd245b4a32
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2016-05-10 09:51:08 +0100

    Updated init to print out information about initial K

--- train/hmmmar_init.m
+++ train/hmmmar_init.m
@@ -25,7 +25,7 @@ Gamma = cell(options.initrep,1);
 parfor it=1:options.initrep
 
     opt_worker = options;
-    opt_worker.Gamma = initGamma_random(T-opt_worker.maxorder,opt_worker.K,opt_worker.DirichletDiag);
+    [opt_worker.Gamma,initial_K] = initGamma_random(T-opt_worker.maxorder,opt_worker.K,opt_worker.DirichletDiag);
     hmm0=struct('train',struct());
     hmm0.K = opt_worker.K;
     hmm0.train = options; 
@@ -36,13 +36,14 @@ parfor it=1:options.initrep
     [hmm0,residuals0]=obsinit(data,T,hmm0,opt_worker.Gamma);
     [~,Gamma{it},~,fehist0] = hmmtrain(data,T,hmm0,opt_worker.Gamma,residuals0);
     fehist(it) = fehist0(end);
-    if size(Gamma{it},2)<opt_worker.K
-        Gamma{it} = [Gamma{it} 0.0001*rand(size(Gamma{it},1),opt_worker.K-size(Gamma{it},2))];
-        Gamma{it} = Gamma{it} ./ repmat(sum(Gamma{it},2),1,opt_worker.K);
-    end
 
     if opt_worker.verbose,
-        fprintf('Init run %d, Free Energy %f \n',it,fehist(it));
+        fprintf('Init run %d, %d->%d states, Free Energy = %f \n',it,initial_K,size(Gamma{it},2),fehist(it));
+    end
+
+    if size(Gamma{it},2)<opt_worker.K % If states were knocked out, add them back
+        Gamma{it} = [Gamma{it} 0.0001*rand(size(Gamma{it},1),opt_worker.K-size(Gamma{it},2))];
+        Gamma{it} = Gamma{it} ./ repmat(sum(Gamma{it},2),1,opt_worker.K);
     end
 
 end
