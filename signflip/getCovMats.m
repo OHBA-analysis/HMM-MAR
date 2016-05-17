583bd3065bc56db55cdf744cb2ef6087c85f0773
dvidaurre@gmail.com <dvidaurre@gmail.com>
2016-05-17 13:14:03 +0100

    Avoiding xcorr in the sign flipping routines because of memory consumption
    Now it's a bit slower - also, maxlag can safely be set to a higher value

--- signflip/getCovMats.m
+++ signflip/getCovMats.m
@@ -7,11 +7,34 @@ t0 = 0;
 for in=1:N 
     t1 = t0 + T(in);
     Xin = X(t0+1:t1,:); t0 = t1;
+    do_xcorr = (numel(Xin) < 1000000) && (ndim < 20);
     for i=1:ndim, if Flips(in,i)==1, Xin(:,i) = -Xin(:,i); end; end
-    r = xcorr(Xin,maxlag,'coeff'); % xcorr is extremely memory consuming    
-    for j = 0:maxlag
-        CovMats(:,:,j+1,in) = reshape(r(maxlag-j+1,:),[ndim ndim]) ;
-        CovMats(:,:,j+1,in) = CovMats(:,:,j+1,in) - diag(diag(CovMats(:,:,j+1,in)));
-    end    
+    if do_xcorr
+        r = xcorr(Xin,maxlag,'coeff'); % xcorr is extremely memory consuming
+        for j = 0:maxlag
+            CovMats(:,:,j+1,in) = reshape(r(maxlag-j+1,:),[ndim ndim]) ;
+            CovMats(:,:,j+1,in) = CovMats(:,:,j+1,in) - diag(diag(CovMats(:,:,j+1,in)));
+        end
+    else
+        r = lowmem_xcorr(Xin,maxlag);
+        for j = 0:maxlag
+            CovMats(:,:,j+1,in) = r(maxlag-j+1,:,:);
+            CovMats(:,:,j+1,in) = CovMats(:,:,j+1,in) - diag(diag(CovMats(:,:,j+1,in)));
+        end
+    end
 end
+end
+
+
+function C = lowmem_xcorr(X,maxlag)
+ndim = size(X,2);
+C = zeros(2*maxlag+1,ndim,ndim);
+for i=0:maxlag
+    for n1=1:ndim
+        for n2=1:ndim
+            C(maxlag+1+i,n1,n2) = corr(X(1:end-i,n1),X(1+i:end,n2));
+            C(maxlag+1-i,n1,n2) = C(maxlag+1+i,n1,n2);
+        end
+    end
+end     
 end
