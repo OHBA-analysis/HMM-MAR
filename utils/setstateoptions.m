677d03bafe4162115cc465c18bdbe5b398009a8b
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2016-08-09 10:50:50 +0100

    Use cache to increase speed

--- utils/setstateoptions.m
+++ utils/setstateoptions.m
@@ -1,7 +1,12 @@
-if isfield(hmm.state(k),'train') && ~isempty(hmm.state(k).train), train = hmm.state(k).train;
-else train = hmm.train;
-end
-[orders,order] = formorders(train.order,train.orderoffset,train.timelag,train.exptimelag);
+% if isfield(hmm.state(k),'train') && ~isempty(hmm.state(k).train)
+	train = hmm.state(k).train;
+% else 
+	% train = hmm.train;
+% end
+% [orders,order] = formorders(train.order,train.orderoffset,train.timelag,train.exptimelag);
+orders = train.cache.orders;
+order = train.cache.order;
+
 if ~isfield(train,'Sind'), 
     train.Sind = formindexes(orders,train.S); 
     if ~train.zeromean, train.Sind = [true(1,size(train.Sind,2)); train.Sind]; end
