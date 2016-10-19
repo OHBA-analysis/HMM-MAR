c05474753dd17de7e87388814b7ff51f2ad5c142
dvidaurre@gmail.com <dvidaurre@gmail.com>
2016-10-19 12:17:34 +0100

    update on hmmregularized_tf_spectra

--- spectral/hmmregularized_tf_spectra.m
+++ spectral/hmmregularized_tf_spectra.m
@@ -1,15 +1,19 @@
-function [psd_tf,coh_tf,pdc_tf] = hmmregularized_tf_spectra(fit,Gamma)
+function [psd_tf,coh_tf,pdc_tf] = hmmregularized_tf_spectra(fit,Gamma,center)
 % obtains a time-frequency representation of the power spectra density,
 % coherence and PDC - the estimations of which (contained in fit) had to be
 % estimated with hmmspectramar or hmmspectramt. If coherence or PDC are
 % missing from the estimation, the corresponding output arguments are
-% empty. Gamma contains the state time courses. 
+% empty. The argument Gamma contains the state time courses. 
+% If the third argument is 1, then each frequency bin is centered 
+%
 % The output arguments are
 %  psd_tf: (time points by no. of frequency bins by no.regions) 
 %  coh_tf: (time points by no. of frequency bins by no.regions by no.regions) 
 %  pdc_tf: (time points by no. of frequency bins by no.regions by no.regions)
 %
 % Author: Diego Vidaurre, OHBA, University of Oxford (2016)
+
+if nargin<3, center = 0; end
 
 coh_tf = []; pdc_tf = [];
 
@@ -24,6 +28,13 @@ for k=1:K
             repmat(fit.state(k).psd(:,n,n),[1 T])';
     end
 end
+if center
+    for f=1:nf
+        for n=1:ndim
+            psd_tf(:,f,n) = psd_tf(:,f,n) - mean(psd_tf(:,f,n));
+        end
+    end
+end
 
 if isfield(fit.state(1),'coh')
     coh_tf = ones(T,nf,ndim,ndim);
@@ -33,6 +44,16 @@ if isfield(fit.state(1),'coh')
                 coh_tf(:,:,n1,n2) = coh_tf(:,:,n1,n2) + repmat(Gamma(:,k),[1 nf]) .* ...
                     repmat(fit.state(k).coh(:,n1,n2),[1 T])';
                 coh_tf(:,:,n2,n1) = coh_tf(:,:,n1,n2);
+            end
+        end
+    end
+    if center
+        for f=1:nf
+            for n1=1:ndim-1
+                for n2=n1+1:ndim
+                    coh_tf(:,f,n1,n2) = coh_tf(:,f,n1,n2) - mean(coh_tf(:,f,n1,n2));
+                    coh_tf(:,f,n2,n1) = coh_tf(:,f,n1,n2);
+                end
             end
         end
     end
@@ -46,6 +67,16 @@ if isfield(fit.state(1),'pdc')
                 if n1==n2, continue; end
                 pdc_tf(:,:,n1,n2) = pdc_tf(:,:,n1,n2) + repmat(Gamma(:,k),[1 nf]) .* ...
                     repmat(fit.state(k).pdc(:,n1,n2),[1 T])';
+            end
+        end
+    end
+    if center
+        for f=1:nf
+            for n1=1:ndim
+                for n2=1:ndim
+                    if n1==n2, continue; end
+                    pdc_tf(:,f,n1,n2) = pdc_tf(:,f,n1,n2) - mean(pdc_tf(:,f,n1,n2));
+                end
             end
         end
     end
