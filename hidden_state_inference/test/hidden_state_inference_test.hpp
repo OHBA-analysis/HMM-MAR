5510e7ed631b8528f8764f570686c0b3c1c3496b
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2016-11-04 10:18:28 +0000

    Restored original C files for older Matlab

---
+++ hidden_state_inference/test/hidden_state_inference_test.hpp
@@ -0,0 +1,9 @@
+/* hidden_state_inference.hpp */
+
+#ifndef __HIDDEN_STATE_INFERENCE_TEST__
+#define __HIDDEN_STATE_INFERENCE_TEST__
+
+#include "hidden_state_inference.hpp"
+#include "armadillo"
+
+#endif
