7aa55965f9f561cf93e8062e407a7184cc8bc743
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2016-11-04 10:39:11 +0000

    Kept my hidden state inference

--- hidden_state_inference/test/hidden_state_inference_test.hpp
+++
@@ -1,9 +0,0 @@
-/* hidden_state_inference.hpp */
-
-#ifndef __HIDDEN_STATE_INFERENCE_TEST__
-#define __HIDDEN_STATE_INFERENCE_TEST__
-
-#include "hidden_state_inference.hpp"
-#include "armadillo"
-
-#endif
