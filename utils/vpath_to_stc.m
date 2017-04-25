17d2b209e629ca49b2ba76c15a5f7a4d3ecbc23d
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-04-25 08:56:59 +0100

    Bug in vpath_to_stc.m

--- utils/vpath_to_stc.m
+++ utils/vpath_to_stc.m
@@ -1,7 +1,7 @@
 function Gamma = vpath_to_stc(vpath)
 % changes a viterbi path (T x 1) to a state tiem course (T x K) format
 
-if size(vpath,1)>1, error('Viterbi paths have one single column'); end
+if size(vpath,2)>1, error('Viterbi paths have one single column'); end
 K = length(unique(vpath));
 Gamma = zeros(numel(vpath),K,'single');
 for k=1:K, Gamma(vpath==k,k) = 1; end        
