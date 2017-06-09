cb350d16e9fb740477d2cb49f801b71ae8547749
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-06-09 18:32:34 +0100

    Made GammaEntropy.m more robust to extreme data

--- eval/GammaEntropy.m
+++ eval/GammaEntropy.m
@@ -1,18 +1,24 @@
 function Entr = GammaEntropy(Gamma,Xi,T,order)
 % Entropy of the state time courses
 Entr = 0; K = size(Gamma,2);
-for tr=1:length(T);
+for tr=1:length(T)
     t = sum(T(1:tr-1)) - (tr-1)*order + 1;
-    Gamma_nz = Gamma(t,:); Gamma_nz(Gamma_nz==0) = realmin;
+    Gamma_nz = Gamma(t,:); 
+    Gamma_nz(Gamma_nz==0) = realmin;
+    if any(isinf(log(Gamma_nz(:)))), Gamma_nz(Gamma_nz==0) = eps; end
     Entr = Entr - sum(Gamma_nz.*log(Gamma_nz));
     t = (sum(T(1:tr-1)) - (tr-1)*(order+1) + 1) : ((sum(T(1:tr)) - tr*(order+1)));
-    Xi_nz = Xi(t,:,:); Xi_nz(Xi_nz==0) = realmin;
+    Xi_nz = Xi(t,:,:);
+    Xi_nz(Xi_nz==0) = realmin;
+    if any(isinf(log(Xi_nz(:)))), Xi_nz(Xi_nz==0) = eps; end
     Psi=zeros(size(Xi_nz));                    % P(S_t|S_t-1)
-    for k = 1:K,
+    for k = 1:K
         sXi = sum(permute(Xi_nz(:,k,:),[1 3 2]),2);
         Psi(:,k,:) = Xi_nz(:,k,:)./repmat(sXi,[1 1 K]); 
     end
     Psi(Psi==0) = realmin;
+    if any(isinf(log(Psi(:)))), Psi(Psi==0) = eps; end
     Entr = Entr - sum(Xi_nz(:).*log(Psi(:)));    % entropy of hidden states
 end
+if isnan(Entr(:)), keyboard; end
 end
