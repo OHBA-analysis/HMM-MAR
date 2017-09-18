cbad020fee58b41d7758b25442a936d08b5e3b8d
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-09-18 18:31:51 +0100

    Update on simhmmmar.m

--- simulate/simhmmmar.m
+++ simulate/simhmmmar.m
@@ -1,4 +1,4 @@
-function [X,T,Gamma] = simhmmmar(T,hmm,Gamma,nrep,trim,X0,sim_state_tcs_only,grouping)
+function [X,T,Gamma] = simhmmmar(T,hmm,Gamma,nrep,sim_state_tcs_only,grouping)
 %
 % Simulate data from the HMM-MAR
 %
@@ -8,7 +8,6 @@ function [X,T,Gamma] = simhmmmar(T,hmm,Gamma,nrep,trim,X0,sim_state_tcs_only,gro
 % hmm                   hmm structure with options specified in hmm.train
 % Gamma                 State courses - leave these empty to simulate these too
 % nrep                  no. repetitions of Gamma(t), from which we take the average
-% trim                  how many time points to remove from the beginning of each trial
 % X0                    A starting point for the time series ( no. time points x ndim x length(T) )
 %                       - if not provided, it is set to Gaussian noise
 % sim_state_tcs_only    Flag to indicate that only state time courses will be
@@ -22,29 +21,35 @@ function [X,T,Gamma] = simhmmmar(T,hmm,Gamma,nrep,trim,X0,sim_state_tcs_only,gro
 % Author: Diego Vidaurre, OHBA, University of Oxford
 
 N = length(T); K = length(hmm.state);
-ndim = size(hmm.state(1).W.Mu_W,2); 
+ndim = size(hmm.state(1).W.Mu_W,2);
 
 if nargin<3, Gamma = []; end
 if nargin<4 || isempty(nrep), nrep = 10; end
-if nargin<5 || isempty(trim), trim = 0; end
-if nargin<6, X0 = []; end
-if nargin<7, sim_state_tcs_only=0; end
-if nargin<8, grouping=[]; end
+if nargin<5, sim_state_tcs_only=0; end
+if nargin<6, grouping=[]; end
     
 if ~isfield(hmm.train,'timelag'), hmm.train.timelag = 1; end
 if ~isfield(hmm.train,'exptimelag'), hmm.train.exptimelag = 1; end
 if ~isfield(hmm.train,'orderoffset'), hmm.train.orderoffset = 0; end
 if ~isfield(hmm.train,'S'), hmm.train.S = ones(ndim); end
 if ~isfield(hmm.train,'multipleConf'), hmm.train.multipleConf = 0; end
+if ~isfield(hmm.train,'maxorder'), hmm.train.maxorder = hmm.train.order; end
+
+if hmm.train.maxorder > 0, d = 500;
+else, d = 0;
+end
+
 
 if isempty(Gamma) && K>1 % Gamma is not provided, so we simulate it too
     Gamma = simgamma(T,hmm.P,hmm.Pi,nrep,grouping);
 elseif isempty(Gamma) && K==1
     Gamma = ones(sum(T),1);
 end
+if size(Gamma,1) ~= sum(T), error('Gamma should have sum(T) rows'); end
 
-X = [];
-if ~isfield(hmm.train,'maxorder'), hmm.train.maxorder = hmm.train.order; end
+X = zeros(sum(T)-N*hmm.train.maxorder,ndim);
+ind = false(sum(T),1);
+hmm0 = hmm; hmm0.train.zeromean = 1; 
 
 if ~sim_state_tcs_only
     for k=1:K
@@ -61,18 +66,17 @@ if ~sim_state_tcs_only
     end
     for n = 1:N
         t0 = sum(T(1:n-1)) + 1; t1 = sum(T(1:n));
-        if isempty(X0)
-            Xin = simgauss(T(n),hmm,Gamma(t0:t1,:));
-            start = hmm.train.maxorder + 1; 
-        else
-            Xin = zeros(T(n),ndim);
-            start = size(X0,1);
-            Xin(1:start,:) = X0(:,:,n);
-            Xin(start+1:end,:) = simgauss(T(n)-start,hmm,Gamma(t0+start:t1,:));
-            start = start + 1; 
+        if hmm.train.maxorder > 0 % a MAR is generating the data
+            Gamma0 = simgamma(d,hmm.P,hmm.Pi,nrep,grouping);
+            X0 = simgauss(d,hmm0,Gamma0); % no mean in the innovation signal
+        else % sampling Gaussian
+            X0 = [];
         end
+        start = hmm.train.maxorder + 1;
+        Xin = [X0; simgauss(T(n),hmm,Gamma(t0:t1,:))]; 
         if ~hmm.train.zeromean || hmm.train.maxorder > 0
-            for t=start:T(n)
+            G = [Gamma0; Gamma(t0:t1,:)];
+            for t=start:T(n)+d
                 for k=1:K
                     orders = hmm.state(k).train.orders;
                     XX = zeros(1,length(orders)*ndim);
@@ -83,29 +87,18 @@ if ~sim_state_tcs_only
                     if ~hmm.train.zeromean
                         XX = [1 XX];
                     end
-                    Xin(t,:) = Xin(t,:) + Gamma(t,k) * XX * hmm.state(k).W.Mu_W;
+                    Xin(t,:) = Xin(t,:) + G(t,k) * XX * hmm.state(k).W.Mu_W;
                 end
             end
         end
-        X = [X; Xin];
+        ind(t0+hmm.train.maxorder : t1) = true;
+        t0 = sum(T(1:n-1)) + 1 - (n-1)*hmm.train.maxorder; 
+        t1 = sum(T(1:n)) - n*hmm.train.maxorder; 
+        X(t0:t1,:) = Xin(d+1+hmm.train.maxorder:end,:);
     end
 end
 
-if trim>0
-    Gamma0 = []; X0 = [];  
-    for n=1:N
-        t0 = sum(T(1:n-1)) + 1; t1 = sum(T(1:n));
-        Gamma0 = [Gamma0; Gamma(t0+trim:t1,:)];
-        if ~sim_state_tcs_only
-            X0 = [X0; X(t0+trim:t1,:)];
-        end
-        T(n) = T(n) - trim;
-    end
-    Gamma = Gamma0; 
-    if ~sim_state_tcs_only
-        X = X0;
-    end
-end
+Gamma = Gamma(ind,:);
 
 
 end
