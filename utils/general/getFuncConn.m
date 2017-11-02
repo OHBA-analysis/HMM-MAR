eaf4c7b7b7e4031e409563e109fe980f8eaac366
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-11-02 10:23:04 +0000

    Rename, documented and polished function for extracting FC

--- utils/general/getFuncConn.m
+++ utils/general/getFuncConn.m
@@ -1,6 +1,12 @@
-function covmat = getCovMat(hmm,k)
-% hmm is the estimated HMMMAR model
-% k is the state of interest
+function [covmat,corrmat] = getFuncConn(hmm,k)
+% Get the covariance and correlation matrices for state k, 
+%   from the estimated model hmm
+% If order==0 (Gaussian distribution), these purely represents functional
+%   connectivity
+% If order>0 (MAR), these refer to the covariance matrix of the residual
+%
+% Diego Vidaurre, OHBA, University of Oxford (2016)
+
 if ~isfield(hmm.state(1),'Omega')
     error('The model was defined with a unique covariance matrix'); 
 end
@@ -14,4 +20,5 @@ else
     ndim = length(hmm.state(k).Omega.Gam_rate);
     covmat = hmm.state(k).Omega.Gam_rate / (hmm.state(k).Omega.Gam_shape-ndim-1);
 end
+corrmat = corrcov(covmat);
 end
