96f01174b7aa6eceec689aa91fd3ab09578d161b
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-12-06 13:18:15 +0000

    Reallocate TUDA examples

--- examples/run_TUDA.m
+++ examples/run_TUDA.m
@@ -1,5 +1,5 @@
+%% Example for a continuous response that varies across time
 
-%% Generate some data
 addpath(genpath('.'))
 
 K =3; p = 10; q = 3; 
@@ -45,14 +45,69 @@ subplot(2,2,3); plot(squeeze(mean(g,2)))
 subplot(2,2,2); plot(Gamma(1:ttrial,:))
 subplot(2,2,4); plot(squeeze(g(:,1,:)))
 
-
-%% 
-
 options.NCV = 5;
 options.lossfunc = 'quadratic';
 R2 = tudacv(X,Y,T,options);
-figure(4);clf(4)
+figure
 plot(R2)
+
+
+
+%% Example for a response that varies trial by trial but stays fixed within trial
+% As in Myers et al (2015) 
+
+
+addpath(genpath('.'))
+
+K =3; p = 10; q = 3; 
+N = 1000;
+ttrial = 150;
+
+% random regression coefficients
+beta = rand(q,p,K);
+
+X = zeros(ttrial,N,p); % data
+Y = zeros(ttrial,N,q); % stimulus
+g = zeros(ttrial,N,K); % state time courses
+T = ttrial * ones(N,1); 
+for n = 1:N
+    t = 1; k = 1; 
+    while t<ttrial
+        l = round(20 * rand) + 10;
+        t2 = t+l-1; 
+        if t2>ttrial, t2 = ttrial; l = t2-t+1; end
+        g(t:t2,n,k) = 1;
+        Y(t:t2,n,:) = repmat(rand(1,q),l,1);
+        X(t:t2,n,:) = squeeze(Y(t:t2,n,:)) * beta(:,:,k) + 0.1 * randn(t2-t+1,p); 
+        t = t + l;
+        k = k + 1;
+        if k > K, k = 1; end
+    end
+end
+    
+X = reshape(X,[ttrial*N p]);
+Y = reshape(Y,[ttrial*N q]);
+
+options = struct();
+options.K = K;
+options.verbose = 1;
+[tuda,Gamma] = tudatrain(X,Y,T,options);
+R2 = tudacv(X,Y,T,options);
+
+% if the distinction between the 3 models is not very clear, it's because
+% two of the columns of beta turned out to be too similar. You might just
+% want to rerun
+%%
+figure(1); 
+subplot(2,2,1); plot(squeeze(mean(reshape(Gamma,[ttrial N K]),2)),'LineWidth',3); ylim([-0.05 1.05])
+subplot(2,2,3); plot(squeeze(mean(g,2)),'LineWidth',3); ylim([-0.05 1.05])
+subplot(2,2,2); plot(Gamma(1:ttrial,:),'LineWidth',3); ylim([-0.05 1.05])
+subplot(2,2,4); plot(squeeze(g(:,1,:)),'LineWidth',3); ylim([-0.05 1.05])
+
+options.NCV = 5;
+options.lossfunc = 'quadratic';
+figure(2)
+plot(R2,'LineWidth',3);% ylim([-0.05 1.05])
 
 %% Just another example
 
