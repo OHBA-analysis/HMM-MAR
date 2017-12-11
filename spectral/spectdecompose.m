9ab38b12b1e4564fdd73f26c2d66054a8d4ebdf3
dvidaurre@gmail.com <dvidaurre@gmail.com>
2017-12-11 14:28:42 +0000

    Corrected version of spectdecompose

--- spectral/spectdecompose.m
+++ spectral/spectdecompose.m
@@ -40,13 +40,14 @@ if ~iscell(sp_fit)
     error('Variable fit needs to be a cell, with one estimation per subject')
 end
 
-if nargin < 3, options = struct(); end
+if nargin < 2, options = struct(); end
 if ~isfield(options,'Ncomp'), options.Ncomp = 4; end
 if ~isfield(options,'Method'), options.Method = 'NNMF'; end
 if ~isfield(options,'Base'), options.Base = 'coh'; end
 if ~isfield(options,'plot'), options.plot = 1; end
 
 p = size(sp_fit{1}.state(1).psd,2); % no. channels
+p2 = p*(p-1)/2;
 Nf = size(sp_fit{1}.state(1).psd,1); % no. frequencies
 N = length(sp_fit); % no. subjects
 K = length(sp_fit{1}.state); % no. states
@@ -68,45 +69,46 @@ for n = 1:N
     end
 end
 
+% Build the matrix that is to be factorised
+Xpsd = zeros(Nf,p*K);
+for k=1:K
+    ind = (1:p) + (k-1)*p;
+    Xpsd(:,ind)= squeeze(mean(abs(psd_comps(:,k,:,:)),1));
+end
+Xcoh = zeros(Nf,K*p2);
+for k = 1:K
+    ind = (1:p2) + (k-1)*p2;
+    ck = squeeze(mean(abs(coh_comps(:,k,:,:,:)),1));
+    Xcoh(:,ind) = ck(:,ind_offdiag);
+end
+if strcmpi(options.Base,'psd')
+    X = Xpsd;
+else
+    X = Xcoh;
+end
+
 if ~isfield(options,'sp_profiles') || isempty(options.sp_profiles)
-    
-    % Build the matrix that is to be factorised
-    if strcmpi(options.Base,'psd')
-        X = zeros(Nf,p*K);
-        for k=1:K
-            ind = (1:p) + (k-1)*p;
-            X(:,ind)= squeeze(mean(abs(psd_comps(:,k,:,:)),1));
-        end
-    else
-        p2 = p*(p-1)/2;
-        X = zeros(Nf,K*p2); 
-        for k = 1:K
-            ind = (1:p2) + (k-1)*p2;
-            ck = squeeze(mean(abs(coh_comps(:,k,:,:,:)),1));
-            X(:,ind) = ck(:,ind_offdiag);
-        end
-    end
-    
     % Doing the decomposition
     if strcmpi(options.Method,'NNMF')
         try
-            [~,b] = nnmf(X,options.Ncomp,'replicates',500,'algorithm','als');
+            [a,~] = nnmf(X,options.Ncomp,'replicates',500,'algorithm','als');
         catch
             error('nnmf not found - perhaps the Matlab version is too old')
         end
-        sp_profiles = pinv(X') * b';
+        %sp_profiles = pinv(X') * b'; % you lose non-negativity by doing this
+        sp_profiles = a; 
     else
         try
-            [~,b] = pca(X,'NumComponents',options.Ncomp);
+            m = mean(X); 
+            X = X - repmat(m,Nf,1);
+            [b,a] = pca(X,'NumComponents',options.Ncomp);
         catch
             error('Error running pca - maybe not matlab''s own?')
         end
-        sp_profiles = b';
+        sp_profiles = a;
     end
-
 else   
     sp_profiles = options.sp_profiles; 
-   
 end
 
 % plot if required
@@ -126,38 +128,78 @@ end
 % group level
 sp_fit_group = struct();
 sp_fit_group.state = struct();
+% NNMF / PCA
+if strcmpi(options.Method,'NNMF')
+    [~,b] = nnmf(Xpsd,options.Ncomp,'replicates',500,'algorithm','als',...
+        'w0',sp_profiles);
+    psd = b'; % regions by components  
+    [~,b] = nnmf(Xcoh,options.Ncomp,'replicates',500,'algorithm','als',...
+        'w0',sp_profiles);
+    coh = b'; % pairs of regions by components   
+else
+    Xpsd = Xpsd - repmat(mean(Xpsd),Nf,1);
+    Xcoh = Xcoh - repmat(mean(Xcoh),Nf,1);
+    psd = (Xpsd' * sp_profiles);
+    coh = (Xcoh' * sp_profiles);
+end
 for k = 1:K
-    psd = squeeze(mean(abs(psd_comps(:,k,:,:)),1))' *  sp_profiles;
-    coh = squeeze(mean(abs(coh_comps(:,k,:,:,:)),1));
-    coh = (coh(:,ind_offdiag)' * sp_profiles)';
     sp_fit_group.state(k).psd = zeros(options.Ncomp,p,p);
     sp_fit_group.state(k).coh = ones(options.Ncomp,p,p);
-    for j = 1:p
-        sp_fit_group.state(k).psd(:,j,j) = psd(j,:)';
+    ind = (1:p) + (k-1)*p;
+    for i = 1:options.Ncomp
+        sp_fit_group.state(k).psd(i,:,:) = diag(psd(ind,i));
     end
+    ind = (1:p2) + (k-1)*p2;
     for i = 1:options.Ncomp
         graphmat = zeros(p);
-        graphmat(ind_offdiag) = coh(i,:);
+        graphmat(ind_offdiag) = coh(ind,i);
         graphmat=(graphmat+graphmat') + eye(p);
         sp_fit_group.state(k).coh(i,:,:) = graphmat;
     end
 end
 
+% Subject level
 for n = 1:N
     sp_fit{n} = struct();
     sp_fit{n}.state = struct();
+    % prepare matrix
+    Xpsd = zeros(Nf,p*K);
+    for k=1:K
+        ind = (1:p) + (k-1)*p;
+        Xpsd(:,ind)= squeeze(abs(psd_comps(n,k,:,:)));
+    end
+    Xcoh = zeros(Nf,K*p2);
     for k = 1:K
-        psd = squeeze(abs(psd_comps(n,k,:,:)))' *  sp_profiles;
-        coh = squeeze(abs(coh_comps(n,k,:,:,:)));
-        coh = (coh(:,ind_offdiag)' * sp_profiles)';
+        ind = (1:p2) + (k-1)*p2;
+        ck = squeeze(abs(coh_comps(n,k,:,:,:)));
+        Xcoh(:,ind) = ck(:,ind_offdiag);
+    end
+    % NNMF / PCA
+    if strcmpi(options.Method,'NNMF')
+        [~,b] = nnmf(Xpsd,options.Ncomp,'replicates',500,'algorithm','als',...
+            'w0',sp_profiles);
+        psd = b'; % regions by components
+        [~,b] = nnmf(Xcoh,options.Ncomp,'replicates',500,'algorithm','als',...
+            'w0',sp_profiles);
+        coh = b';
+    else
+        Xpsd = Xpsd - repmat(mean(Xpsd),Nf,1);
+        Xcoh = Xcoh - repmat(mean(Xcoh),Nf,1);
+        psd = (Xpsd' * sp_profiles);
+        coh = (Xcoh' * sp_profiles);
+    end
+    % Reshape stuff
+    for k = 1:K
         sp_fit{n}.state(k).psd = zeros(options.Ncomp,p,p);
         sp_fit{n}.state(k).coh = ones(options.Ncomp,p,p);
-        for j = 1:p
-            sp_fit{n}.state(k).psd(:,j,j) = psd(j,:)';
+        ind = (1:p) + (k-1)*p;
+        for i = 1:options.Ncomp
+            sp_fit{n}.state(k).psd(i,:,:) = diag(psd(ind,i));
         end
+        ind = (1:p2) + (k-1)*p2;
         for i = 1:options.Ncomp
             graphmat = zeros(p);
-            graphmat(ind_offdiag) = coh(i,:);
+            graphmat(ind_offdiag) = coh(ind,i);
             graphmat=(graphmat+graphmat') + eye(p);
             sp_fit{n}.state(k).coh(i,:,:) = graphmat;
         end
@@ -165,4 +207,5 @@ for n = 1:N
 end
     
 end
+
 
