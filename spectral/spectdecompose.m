886ae459e1486d21f53b011a38286c6458cee555
Diego Vidaurre <vidaurre@fmrib.ox.ac.uk>
2018-04-20 14:48:55 +0900

    ratlonalise spectdecompose when N = 1

--- spectral/spectdecompose.m
+++ spectral/spectdecompose.m
@@ -26,9 +26,12 @@ function [sp_fit,sp_fit_group,sp_profiles] = spectdecompose(sp_fit,options)
 % 
 % sp_fit                A cell with the spectral components, with the same fields  
 %                       and dimensions that the input 'sp_fit', but with Ncomp 
-%                       components instead of No. frequency bins.
+%                       components instead of No. frequency bins. If there
+%                       is only one subject, then sp_field is a single struct
+%                 
 % sp_fit_group          A single struct containing the mean across subjects
-%                       of sp_fit
+%                       of sp_fit. If there is only one subject, then
+%                       sp_fit_group equals sp_field
 % sp_profiles           The (frequency bins by spectral components) mixing
 %                       matrix used to project from (no. of frequency bins
 %                       by regions) to (no. of components by regions)
@@ -106,10 +109,10 @@ if ~isfield(options,'sp_profiles') || isempty(options.sp_profiles)
             error('nnmf not found - perhaps the Matlab version is too old')
         end
         %sp_profiles = pinv(X') * b'; % you lose non-negativity by doing this
-        sp_profiles = a; 
+        sp_profiles = a;
     else
         try
-            m = mean(X); 
+            m = mean(X);
             X = X - repmat(m,Nf,1);
             [b,a] = pca(X,'NumComponents',options.Ncomp);
         catch
@@ -117,17 +120,17 @@ if ~isfield(options,'sp_profiles') || isempty(options.sp_profiles)
         end
         sp_profiles = a;
     end
-else   
-    sp_profiles = options.sp_profiles; 
+else
+    sp_profiles = options.sp_profiles;
 end
 
 % plot if required
 if options.plot
     figure;
     if options.Ncomp > 4
-        j1 = ceil(options.Ncomp/2); j2 = 2; 
+        j1 = ceil(options.Ncomp/2); j2 = 2;
     else
-        j1 = options.Ncomp; j2 = 1; 
+        j1 = options.Ncomp; j2 = 1;
     end
     for j = 1:options.Ncomp
         subplot(j1,j2,j)
@@ -140,13 +143,13 @@ sp_fit_group = struct();
 sp_fit_group.state = struct();
 % NNMF / PCA
 if strcmpi(options.Method,'NNMF')
-    opt = statset('maxiter',1);    
+    opt = statset('maxiter',1);
     [~,b] = nnmf(Xpsd,options.Ncomp,'algorithm','als',...
         'w0',sp_profiles,'Options',opt);
-    psd = b'; % regions by components  
+    psd = b'; % regions by components
     [~,b] = nnmf(Xcoh,options.Ncomp,'algorithm','als',...
         'w0',sp_profiles,'Options',opt);
-    coh = b'; % pairs of regions by components   
+    coh = b'; % pairs of regions by components
 else
     Xpsd = Xpsd - repmat(mean(Xpsd),Nf,1);
     Xcoh = Xcoh - repmat(mean(Xcoh),Nf,1);
@@ -170,54 +173,58 @@ for k = 1:K
 end
 
 % Subject level
-for n = 1:N
-    sp_fit{n} = struct();
-    sp_fit{n}.state = struct();
-    % prepare matrix
-    Xpsd = zeros(Nf,ndim*K);
-    for k=1:K
-        ind = (1:ndim) + (k-1)*ndim;
-        Xpsd(:,ind)= squeeze(abs(psd_comps(n,k,:,:)));
-    end
-    Xcoh = zeros(Nf,K*ndim2);
-    for k = 1:K
-        ind = (1:ndim2) + (k-1)*ndim2;
-        ck = squeeze(abs(coh_comps(n,k,:,:,:)));
-        Xcoh(:,ind) = ck(:,ind_offdiag); 
-    end
-    % NNMF / PCA
-    if strcmpi(options.Method,'NNMF')
-        opt = statset('maxiter',1);    
-        [~,b] = nnmf(Xpsd,options.Ncomp,'algorithm','als',...
-            'w0',sp_profiles,'Options',opt);
-        psd = b'; % regions by components
-        [~,b] = nnmf(Xcoh,options.Ncomp,'algorithm','als',...
-            'w0',sp_profiles,'Options',opt);
-        coh = b';
-    else
-        Xpsd = Xpsd - repmat(mean(Xpsd),Nf,1);
-        Xcoh = Xcoh - repmat(mean(Xcoh),Nf,1);
-        psd = (Xpsd' * sp_profiles);
-        coh = (Xcoh' * sp_profiles);
-    end
-    % Reshape stuff
-    for k = 1:K
-        sp_fit{n}.state(k).psd = zeros(options.Ncomp,ndim,ndim);
-        sp_fit{n}.state(k).coh = ones(options.Ncomp,ndim,ndim);
-        ind = (1:ndim) + (k-1)*ndim;
-        for i = 1:options.Ncomp
-            sp_fit{n}.state(k).psd(i,:,:) = diag(psd(ind,i));
+if N>1
+    for n = 1:N
+        sp_fit{n} = struct();
+        sp_fit{n}.state = struct();
+        % prepare matrix
+        Xpsd = zeros(Nf,ndim*K);
+        for k=1:K
+            ind = (1:ndim) + (k-1)*ndim;
+            Xpsd(:,ind)= squeeze(abs(psd_comps(n,k,:,:)));
         end
-        ind = (1:ndim2) + (k-1)*ndim2;
-        for i = 1:options.Ncomp
-            graphmat = zeros(ndim);
-            graphmat(ind_offdiag) = coh(ind,i);
-            graphmat=(graphmat+graphmat') + eye(ndim);
-            sp_fit{n}.state(k).coh(i,:,:) = graphmat;
+        Xcoh = zeros(Nf,K*ndim2);
+        for k = 1:K
+            ind = (1:ndim2) + (k-1)*ndim2;
+            ck = squeeze(abs(coh_comps(n,k,:,:,:)));
+            Xcoh(:,ind) = ck(:,ind_offdiag);
+        end
+        % NNMF / PCA
+        if strcmpi(options.Method,'NNMF')
+            opt = statset('maxiter',1);
+            [~,b] = nnmf(Xpsd,options.Ncomp,'algorithm','als',...
+                'w0',sp_profiles,'Options',opt);
+            psd = b'; % regions by components
+            [~,b] = nnmf(Xcoh,options.Ncomp,'algorithm','als',...
+                'w0',sp_profiles,'Options',opt);
+            coh = b';
+        else
+            Xpsd = Xpsd - repmat(mean(Xpsd),Nf,1);
+            Xcoh = Xcoh - repmat(mean(Xcoh),Nf,1);
+            psd = (Xpsd' * sp_profiles);
+            coh = (Xcoh' * sp_profiles);
+        end
+        % Reshape stuff
+        for k = 1:K
+            sp_fit{n}.state(k).psd = zeros(options.Ncomp,ndim,ndim);
+            sp_fit{n}.state(k).coh = ones(options.Ncomp,ndim,ndim);
+            ind = (1:ndim) + (k-1)*ndim;
+            for i = 1:options.Ncomp
+                sp_fit{n}.state(k).psd(i,:,:) = diag(psd(ind,i));
+            end
+            ind = (1:ndim2) + (k-1)*ndim2;
+            for i = 1:options.Ncomp
+                graphmat = zeros(ndim);
+                graphmat(ind_offdiag) = coh(ind,i);
+                graphmat=(graphmat+graphmat') + eye(ndim);
+                sp_fit{n}.state(k).coh(i,:,:) = graphmat;
+            end
         end
     end
+else
+    sp_fit = sp_fit_group;
 end
-    
+
 end
 
 
