928deaaae9c078108e5ef37a69d96552f1d8e287
Cameron Higgins <Cameron Higgins>
2018-05-17 17:02:51 +0100

    minor changes to checkGammaOverfitting

--- utils/internal/checkGammaOverfitting.m
+++ utils/internal/checkGammaOverfitting.m
@@ -1,19 +1,21 @@
-function Gamma = checkGammaOverfitting(Gamma,residuals,hmm,T,orders)
+function [Gamma,D] = checkGammaOverfitting(Gamma,residuals,hmm,T,orders)
 % Check whether a given timecourse is overfitting to the data by assigning
 % anticorrelated state timecourses to different conditions. If overfitting
 % is occuring for a particular label, that label's timecourse is reset to
 % the global mean value of Gamma.
-
+if nargin<5
+    orders=0;
+end
 P=T(1)-orders; 
-
+Ydim = size(residuals,2);
 GammaMean(1,:,:)=squeeze(mean(reshape(Gamma(residuals(:,1)~=1,:),P,sum(residuals(:,1)~=1)/P,hmm.train.K),2));
-for k=1:hmm.train.logisticYdim
+for k=1:Ydim%hmm.train.logisticYdim
     GammaMean(k+1,:,:)=squeeze(mean(reshape(Gamma(residuals(:,k)==1,:),P,sum(residuals(:,k)==1)/P,hmm.train.K),2));
 end
 
 %D=dist(GammaMean(:,:)')./size(GammaMean,2);
-for k=1:hmm.train.logisticYdim+1
-    for l=k:hmm.train.logisticYdim+1
+for k=1:Ydim+1
+    for l=k:Ydim+1
         for st = 1:hmm.train.K
             rho = corr([squeeze(GammaMean(k,:,st));squeeze(GammaMean(l,:,st))]');
             Rmat(k,l,st)=rho(2,1);
