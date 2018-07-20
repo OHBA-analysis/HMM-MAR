555aa8abe9b8d63d16f8d357da582f48278e8504
Cameron Higgins <Cameron Higgins>
2018-07-20 14:35:29 +0100

    Extended encoding model inversion code

--- task/invertEncodeModel.m
+++ task/invertEncodeModel.m
@@ -1,9 +1,9 @@
 function [Y_pred, loglikelihoodX] = invertEncodeModel(model,Gamma,X)
 % For an inferred encoding model and state timecourses, computes the
-% likelihood manifold in X space and computes the liklihood function for
+% likelihood manifold in X space and computes the likelihood function for
 % each class of labels.
-[T,K]=size(Gamma);
-nDim = size(X,2);
+[T2,K]=size(Gamma);
+[T,nDim] = size(X);
 for k=1:K
     betas_mu{k}=model.state(k).W.Mu_W((nDim+1):end,1:nDim);
     betas_sigma{k}=model.state(k).W.S_W(1:nDim,(nDim+1):end,(nDim+1):end); 
@@ -16,31 +16,47 @@ for testcond = 1:Ydim
     % compute each state's distribution in x space
     for k=1:K
         betamu_givenY(testcond,k,:) = betas_mu{k}(1,:)+betas_mu{k}(1+testcond,:);
-%         betasig_givenY(testcond,k,:,:) = diag(diag(squeeze(betas_sigma{k}(testcond,2:end,2:end)))) + ...
-%             diag(betas_sigma{k}(testcond,1,1)+2*squeeze(betas_sigma{k}(testcond,1,2:end)));
         betasig_givenY(testcond,k,:,:) = diag(betas_sigma{k}(:,testcond+1,testcond+1)) + ...
             diag(betas_sigma{k}(:,1,1))+2*diag(betas_sigma{k}(:,1,testcond+1));
     end
 end
 
 % iterate through time, computing likelihood of X given Gamma:
-Omega = diag(model.Omega.Gam_shape ./ model.Omega.Gam_rate(1:nDim));
-for t=1:T
-    for testcond = 1:Ydim
-        mu = sum(squeeze(betamu_givenY(testcond,:,:)) .*repmat(Gamma(t,:)',1,nDim));
-        mu_rec(testcond,t,:)=mu;
-        sig = Omega + squeeze(sum(squeeze(betasig_givenY(testcond,:,:,:)).*repmat(Gamma(t,:)',1,nDim,nDim),1));
-        loglikelihoodX(t,testcond)=-0.5*log(det(sig)) -0.5 * (X(t,:)-mu)*inv(sig)*(X(t,:)-mu)';
+if isfield(model,'Omega')
+    if isfield(model.Omega,'pseudomean')
+        Omega = model.Omega.pseudomean(1:nDim,1:nDim);
+    else
+        Omega = diag(model.Omega.Gam_shape ./ model.Omega.Gam_rate(1:nDim));
     end
-    m=max(loglikelihoodX(t,:),[],2);
-    Y_pred(t,:) = loglikelihoodX(t,:)==m;
-    if sum(Y_pred(t,:))>1
-        fprintf('\n\nWARNING: Equal scores achieved for multiple classes\n\n');
-        a=find(Y_pred(t,:));
-        Y_pred(t,a)=0;Y_pred(t,a(randi(length(a))))=1; %randomly silence all but one of these entries 
-        
-    end
+else
+    
 end
+if T==T2
+    for t=1:T
+        for testcond = 1:Ydim
+            mu = sum(squeeze(betamu_givenY(testcond,:,:)) .*repmat(Gamma(t,:)',1,nDim));
+            mu_rec(testcond,t,:)=mu;
+            sig = Omega + squeeze(sum(squeeze(betasig_givenY(testcond,:,:,:)).*repmat(Gamma(t,:)',1,nDim,nDim),1));
+            loglikelihoodX(t,testcond)=-0.5*log(det(sig)) -0.5 * (X(t,:)-mu)*inv(sig)*(X(t,:)-mu)';
+        end
+        m=max(loglikelihoodX(t,:),[],2);
+        Y_pred(t,:) = loglikelihoodX(t,:)==m;
+        if sum(Y_pred(t,:))>1
+            fprintf('\n\nWARNING: Equal scores achieved for multiple classes\n\n');
+            a=find(Y_pred(t,:));
+            Y_pred(t,a)=0;Y_pred(t,a(randi(length(a))))=1; %randomly silence all but one of these entries 
 
-
+        end
+    end
+else
+%implies Gamma invariant, so collapse over time:
+    for testcond = 1:Ydim
+        mu= squeeze(betamu_givenY(testcond,:,:))';
+        mu_rec(testcond,:)=mu;
+        sig = Omega + squeeze(betasig_givenY(testcond,:,:,:));
+        loglikelihoodX(:,testcond)=-0.5*log(det(sig)) -0.5 * sum(((X-repmat(mu,T,1))*inv(sig)).*(X-repmat(mu,T,1)),2);
+    end
+    m=max(loglikelihoodX(:,:),[],2);
+    Y_pred = loglikelihoodX(:,:)==repmat(m,1,Ydim);
+end
 end
