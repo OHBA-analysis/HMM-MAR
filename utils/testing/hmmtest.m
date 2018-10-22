735128303738917482fdc551e9eb781ecb92c3ef
Diego Vidaurre <vidaurre@fmrib.ox.ac.uk>
2018-10-22 11:28:23 +0100

    Factorised the code for permutation testing

--- utils/testing/hmmtest.m
+++ utils/testing/hmmtest.m
@@ -139,10 +139,10 @@ if subjectlevel
         %lt = getStateLifeTimes(g,TperSubj{j},options,threshold_visit,threshold_Gamma);
         %it = getStateIntervalTimes(g,TperSubj{j},options,threshold_visit,threshold_Gamma);
         sr = getSwitchingRate(g,TperSubj{j},options);
-        tests.subjectlevel.p_fractional_occupancy(:,j) = permtest(fo,Yj,Nperm,[],confounds);
-        %tests.subjectlevel.p_life_times(:,j) = permtest(lt,Yj,Nperm,[],confounds);
-        %tests.subjectlevel.p_interval_times(:,j) = permtest(it,Yj,Nperm,[],confounds);
-        tests.subjectlevel.p_switching_rate(j) = permtest(sr,Yj,Nperm,[],confounds);
+        tests.subjectlevel.p_fractional_occupancy(:,j) = permtest_aux(fo,Yj,Nperm,[],confounds);
+        %tests.subjectlevel.p_life_times(:,j) = permtest_aux(lt,Yj,Nperm,[],confounds);
+        %tests.subjectlevel.p_interval_times(:,j) = permtest_aux(it,Yj,Nperm,[],confounds);
+        tests.subjectlevel.p_switching_rate(j) = permtest_aux(sr,Yj,Nperm,[],confounds);
     end
 end
    
@@ -155,69 +155,13 @@ if grouplevel
     %lt = getStateLifeTimes(Gamma,T,options,threshold_visit,threshold_Gamma);
     %it = getStateIntervalTimes(Gamma,T,options,threshold_visit,threshold_Gamma);
     sr = getSwitchingRate(Gamma,T,options);
-    tests.grouplevel.p_fractional_occupancy = permtest(fo,Y,Nperm,Ntrials_per_subject,confounds);
-    %tests.grouplevel.p_life_times(:,j) = permtest(lt,Y,Nperm,Ntrials_per_subject,confounds);
-    %tests.grouplevel.p_interval_times(:,j) = permtest(it,Y,Nperm,Ntrials_per_subject,confounds);
-    tests.grouplevel.p_switching_rate = permtest(sr,Y,Nperm,Ntrials_per_subject,confounds);
+    tests.grouplevel.p_fractional_occupancy = permtest_aux(fo,Y,Nperm,Ntrials_per_subject,confounds);
+    %tests.grouplevel.p_life_times(:,j) = permtest_aux(lt,Y,Nperm,Ntrials_per_subject,confounds);
+    %tests.grouplevel.p_interval_times(:,j) = permtest_aux(it,Y,Nperm,Ntrials_per_subject,confounds);
+    tests.grouplevel.p_switching_rate = permtest_aux(sr,Y,Nperm,Ntrials_per_subject,confounds);
 end
           
 end
-
-
-function pval = permtest(X,D,Nperm,grouping,confounds)
-% permutation testing routine (through regression)
-% X: data
-% D: design matrix
-% Nperm: no. of permutations
-% grouping: the first grouping(1) rows belong to one group, 
-%    the next grouping(2) rows belong to the second group, 
-%    and so on and so on
-% Diego Vidaurre
-
-[N,p] = size(X);
-if nargin<4, grouping = []; end
-if (nargin>4) && ~isempty(confounds)
-    confounds = confounds - repmat(mean(confounds),N,1);
-    X = bsxfun(@minus,X,mean(X));   
-    X = X - confounds * pinv(confounds) * X;
-    D = bsxfun(@minus,D,mean(D));   
-    D = D - confounds * pinv(confounds) * D;    
-end
-
-D = bsxfun(@minus,D,mean(D));   
-X = bsxfun(@minus,X,mean(X));  
-grotperms = zeros(Nperm,p);
-proj = (D' * D + 0.001 * eye(size(D,2))) \ D';  
-
-for perm=1:Nperm
-    if perm==1
-       Xin = X;
-    else
-        %if ~isempty(grouping)
-        %    Xin = zeros(size(X));
-        %    for gr = 1:length(grouping)
-        %        jj = (1:grouping(gr)) + sum(grouping(1:gr-1));
-        %        r = randperm(grouping(gr));
-        %        Xin(jj,:) = X(jj(r),:);
-        %    end
-        %else
-        %    r = randperm(N);
-        %    Xin = X(r,:);
-        %end
-        r = randperm(N);
-        Xin = X(r,:);
-    end
-    beta = proj * Xin;
-    grotperms(perm,:) = sqrt(sum((D * beta - Xin).^2)); 
-end
-
-pval = zeros(p,1);
-for j = 1:p
-    pval(j) = sum(grotperms(:,j)<=grotperms(1,j)) / (Nperm+1);
-end
-    
-end
-
 
 % Silly code to try out hmmtest
 %
