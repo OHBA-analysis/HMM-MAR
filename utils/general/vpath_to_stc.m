10dc7aa0b3e4a1a199f151c1b4e6b2d43926633e
vidaurre <dvidaurre@gmail.com>
2018-12-18 15:05:14 +0000

    Changed a problem with the script that maps Viterbi path to prob state tiem courses

--- utils/general/vpath_to_stc.m
+++ utils/general/vpath_to_stc.m
@@ -1,8 +1,8 @@
-function Gamma = vpath_to_stc(vpath)
+function Gamma = vpath_to_stc(vpath,K)
 % changes a viterbi path (T x 1) to a state time course (T x K) format
 
 if size(vpath,2)>1, error('Viterbi paths have one single column'); end
-K = max(vpath);
+if nargin<2, K = max(vpath); end
 Gamma = zeros(numel(vpath),K,'single');
 for k=1:K, Gamma(vpath==k,k) = 1; end        
 
