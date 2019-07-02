c3c876398904a0866b4b864ed798273a7d65e4a3
vidaurre <dvidaurre@gmail.com>
2019-07-02 12:08:32 +0100

    Let flipdata.m accept also inputs in matrix format

--- utils/signflip/flipdata.m
+++ utils/signflip/flipdata.m
@@ -21,28 +21,36 @@ if nargin<4, outputfile = []; end
 N = length(T); 
 
 if iscell(data)  
-    if isempty(outputfile)
-        if ischar(data{1}) && ~force % check with user
-            while true
-                query = 'The result will overwrite the files, are you sure you want to continue? (0/1) ';
-                answer = input(query);
-                if answer==1 || answer==0, break; end
-                disp('Please respond 0 or 1.')
-            end
-            if answer==0
-                disp('Exiting then...')
-                return;
-            end
+    if isempty(outputfile) && ischar(data{1}) && ~force % check with user
+        while true
+            query = 'The result will overwrite the files, are you sure you want to continue? (0/1) ';
+            answer = input(query);
+            if answer==1 || answer==0, break; end
+            disp('Please respond 0 or 1.')
+        end
+        if answer==0
+            disp('Exiting then...')
+            return;
         end
     end
-    for j = 1:N
-        fsub = data{j};
-        loadfile_sub;
-        X = flipdata_subject(X,T{j},flips(j,:));
-        if ~isempty(outputfile)
-            fsub = outputfile{j};
+    if ~ischar(data{1})
+        for j = 1:N
+            data{j} = flipdata_subject(data{j},T{j},flips(j,:));
+            if ~isempty(outputfile)
+                fsub = outputfile{j};
+                writefile_sub;
+            end
         end
-        writefile_sub;
+    else
+        for j = 1:N
+            fsub = data{j};
+            loadfile_sub;
+            X = flipdata_subject(X,T{j},flips(j,:));
+            if ~isempty(outputfile)
+                fsub = outputfile{j};
+            end
+            writefile_sub;
+        end
     end
 else
     ndim = size(data,2);
