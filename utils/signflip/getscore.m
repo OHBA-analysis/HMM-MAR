85b7e2d02d548130fcc4d18818bd89d5e6c7be89
vidaurre <dvidaurre@gmail.com>
2019-07-25 15:39:56 +0100

    Solve issues with sign flipping (source-reconstructed M/EEG data)

--- utils/signflip/getscore.m
+++
@@ -1,8 +0,0 @@
-function score = getscore(M)
-% get the score from the (flipped) matrices of autocorrelation contained in M
-% M is a matrix (ndim x ndim x lags x subjects) with autocovariances matrix for all subjects
-N = size(M,4); ndim = size(M,1); L = size(M,3);
-M = reshape(M,[(ndim^2)*L N]);
-C = corr(M);
-score = mean(C(triu(true(N),1)));
-end
