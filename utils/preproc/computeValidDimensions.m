c3b6df7ca5d8abcb01bf19a9b3a3f435c5cb924f
Cameron.Higgins <Cameron.Higgins@ohba.ox.ac.uk>
2020-02-04 11:44:20 +0000

    Fixed compatibility issue between S and data normalisation under different conditions

--- utils/preproc/computeValidDimensions.m
+++ utils/preproc/computeValidDimensions.m
@@ -1,21 +1,26 @@
-function valid_dims = computeValidDimensions(data,S)
+function valid_dims = computeValidDimensions(data,options)
 % Evaluate a matrix of connections S and return which elements are to be
 % analysed for variance and standardisation.
-if all(S(:)==1)
-    valid_dims=[1:length(S)];
-else
-    %implies a TUDA/TUCA style setup:
-    lastdatadim = find(diff(any(S==1)),1);
-    % check if intercept term present:
-    if isstruct(data)
-         X = data.X;
+S=options.S;
+if strcmp(options.distribution,'Gaussian')
+    if all(S(:)==1)
+        valid_dims=[1:length(S)];
     else
-         X = data;
+        %implies a TUDA/TUCA style setup:
+        lastdatadim = find(diff(any(S==1)),1);
+        % check if intercept term present:
+        if isstruct(data)
+             X = data.X;
+        else
+             X = data;
+        end
+        if var(X(:,lastdatadim))==0
+            lastdatadim = lastdatadim-1;
+        end
+        valid_dims = [1:lastdatadim];
     end
-    if var(X(:,lastdatadim))==0
-        lastdatadim = lastdatadim-1;
-    end
-    valid_dims = [1:lastdatadim];
+else
+    valid_dims=[];
 end
 
 end
