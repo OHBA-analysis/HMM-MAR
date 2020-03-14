31a0de8bc5b354dbd1ed4edfc94cd17c286d6bef
vidaurre <dvidaurre@gmail.com>
2020-03-14 19:01:19 +0100

    Fixed so that the toolbox can be used to run simple mixtures of models (no HMM)

--- train/id_Gamma_inference.m
+++ train/id_Gamma_inference.m
@@ -1,8 +1,9 @@
 function Gamma = id_Gamma_inference(L,Pi,order)
 % inference for independent samples (ignoring time structure)
 
+T = size(L,1) + order; K = length(Pi);
 Gamma = zeros(T,K);
-Gamma(1+order,:) = repmat(Pi,size(L,1),1) .* L(1+order,:);
+Gamma(1+order:end,:) = repmat(Pi,size(L,1),1) .* L(1+order:end,:);
 Gamma = rdiv(Gamma,sum(Gamma,2));
 
 end
