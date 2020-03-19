521314e88eb4c2f9c282e99b01fd3031ed08d2cb
vidaurre <dvidaurre@gmail.com>
2020-03-19 15:46:04 +0100

    Fixed problem of precision with mixture models (no HMM ones)

--- train/id_Gamma_inference.m
+++ train/id_Gamma_inference.m
@@ -2,6 +2,7 @@ function Gamma = id_Gamma_inference(L,Pi,order)
 % inference for independent samples (ignoring time structure)
 
 T = size(L,1) + order; K = length(Pi);
+L(L<realmin) = realmin;
 Gamma = zeros(T,K);
 Gamma(1+order:end,:) = repmat(Pi,size(L,1),1) .* L(1+order:end,:);
 Gamma = rdiv(Gamma,sum(Gamma,2));
