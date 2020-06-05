30efef6f8536e32dd1fdb7563760282acb3f49af
vidaurre <dvidaurre@gmail.com>
2020-06-05 15:48:30 +0200

    Revert last change

--- utils/preproc/rawsignal2power.m
+++ utils/preproc/rawsignal2power.m
@@ -1,5 +1,5 @@
 function data = rawsignal2power(data,T)
-% Gets power time series using the Hilbert transform,
+% Gets *square of* the power time series using the Hilbert transform,
 % as done in Baker et al (2014), eLife.
 %
 % Diego Vidaurre, OHBA, University of Oxford (2017)
@@ -12,11 +12,11 @@ for n = 1:length(T)
     ind = sum(T(1:n-1))+1:sum(T(1:n));
     if isstruct(data)
         for j = 1:ndim
-            data.X(ind,j) = abs(hilbert(data.X(ind,j))).^2;
+            data.X(ind,j) = abs(hilbert(data.X(ind,j)));
         end
     else
         for j = 1:ndim
-            data(ind,j) = abs(hilbert(data(ind,j))).^2;
+            data(ind,j) = abs(hilbert(data(ind,j)));
         end
     end
 end
