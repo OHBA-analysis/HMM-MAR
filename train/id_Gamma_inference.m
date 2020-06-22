33084159e0aaca37b0932ea1cbaab6f0b0ea28e7
vidaurre <dvidaurre@gmail.com>
2020-06-22 09:23:57 +0200

    Fixed an issue with the iid mixture of models (non-HMM)

--- train/id_Gamma_inference.m
+++ train/id_Gamma_inference.m
@@ -1,10 +1,9 @@
 function Gamma = id_Gamma_inference(L,Pi,order)
 % inference for independent samples (ignoring time structure)
 
-T = size(L,1) + order; K = length(Pi);
 L(L<realmin) = realmin;
-Gamma = zeros(T,K);
-Gamma(1+order:end,:) = repmat(Pi,size(L,1),1) .* L(1+order:end,:);
+Gamma = repmat(Pi,size(L,1),1) .* L;
+Gamma = Gamma(1+order:end,:);
 Gamma = rdiv(Gamma,sum(Gamma,2));
 
 end
