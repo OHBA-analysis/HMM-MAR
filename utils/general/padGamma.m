b1339b16a5944732813959a42e84e98206c7e4e2
vidaurre <dvidaurre@gmail.com>
2020-11-27 17:29:27 +0100

    Adapted padGamma to the case when the HMM is run on downsampled data

--- utils/general/padGamma.m
+++ utils/general/padGamma.m
@@ -1,18 +1,14 @@
-function Gamma = padGamma(Gamma,T,options,do_upsample)
+function Gamma = padGamma(Gamma,T,options)
 % Unless a Gaussian distribution is used to describe the states, the state
 % time courses are shorter than the  data time series.
 % This function adjusts the state time courses to have the same size than
 % the data time series, padding the state time courses with probabilities
 % that correspond to the trial-specific fractional occupancies,
-% and upsampling in case options.downsample was specified and the option
-% up_sample is set to true
-% 
+% and upsampling in case options.downsample was specified
+%
 % Author: Diego Vidaurre, OHBA, University of Oxford (2017)
 
-if nargin < 4, do_upsample = true; end
-
-do_upsample = do_upsample && ...
-    (isfield(options,'downsample') && options.downsample > 0); 
+do_upsample = (isfield(options,'downsample') && options.downsample > 0); 
 
 do_chop = 0;
 if isfield(options,'embeddedlags') && length(options.embeddedlags) > 1
@@ -30,24 +26,20 @@ end
 
 K = size(Gamma,2); offset = sum(d); N = length(T); Tshifted = T - offset;
 
-if isfield(options,'downsample') && options.downsample > 0 
-    r =  options.downsample / options.Fs;
-    if do_upsample
-        Gamma_orig = Gamma;
-        Gamma = zeros(sum(Tshifted),K);
-        Tdown = ceil(r * Tshifted );
-        for j = 1:N
-            t1 = (1:Tdown(j)) + sum(Tdown(1:j-1));
-            t2 = (1:Tshifted(j)) + sum(Tshifted(1:j-1));
-            for k = 1:K
-                g = resample(Gamma_orig(t1,k), options.Fs, options.downsample);
-                if length(g)>length(t2), Gamma(t2,k) = g(1:end-1);
-                else,  Gamma(t2,k) = g;
-                end
+if do_upsample
+    r = options.downsample / options.Fs;
+    Gamma_orig = Gamma;
+    Gamma = zeros(sum(Tshifted),K);
+    Tdown = ceil(r * Tshifted );
+    for j = 1:N
+        t1 = (1:Tdown(j)) + sum(Tdown(1:j-1));
+        t2 = (1:Tshifted(j)) + sum(Tshifted(1:j-1));
+        for k = 1:K
+            g = resample(Gamma_orig(t1,k), options.Fs, options.downsample);
+            if length(g)>length(t2), Gamma(t2,k) = g(1:end-1);
+            else,  Gamma(t2,k) = g;
             end
         end
-    else
-        T = ceil(r * T);
     end
 end
 
